18428fd8eb290b4430eb1c9b82da42a8af97c9fa
JesusSePe <57097178+JesusSePe@users.noreply.github.com>
2020-10-18 17:24:18 +0200

    Update compra.php

--- Botiga/compra.php
+++ Botiga/compra.php
@@ -6,27 +6,29 @@
 	<body>
 		<?php
 		session_start();
-			echo "<p>Products:</p>";
-			echo "<select name='products'>";
-			$array = file('Cataleg.txt');
-			$itemNumber = 0;
-			foreach($array as $product){
-				$data = explode(",", $product);
-				echo "<option value='$itemNumber'>$data[0]</option>";
-			}
-			echo "</select><p>Quantitat</p>";
-			echo "<input type='number' min='0' name='quant'>";
-			echo "<input type='submit' name='Buy' value='Buy'>";
-			/*if (isset($_POST['products'])) {
-				$_SESSION['products'] = $_SESSION['products'];
-				$_SESSION['quant'] = $_SESSION['quant'];
-				foreach($_SESSION['products']){
+			echo "<form method='POST' action='compra.php'>";
+				echo "<p>Products:</p>";
+				echo "<select name='products'>";
+				$array = file('Cataleg.txt');
+				$itemNumber = 0;
+				foreach($array as $product){
+					$data = explode(",", $product);
+					echo "<option value='$itemNumber'>$data[0]</option>";
+					$itemNumber += 1;
+				}
+				echo "</select><p>Quantitat</p>";
+				echo "<input type='number' min='0' name='quant'>";
+				echo "<input type='submit' name='Buy' value='Buy'>";
+			echo "</form>";
+			if( $_SERVER["REQUEST_METHOD"] == "POST") {
+				$_SESSION['products'] += $_SESSION['products'];
+				$_SESSION['quant'] += $_SESSION['quant'];
+				foreach($_SESSION['products'] as $info){
 					echo "<pre>";
-					print_r($_SESSION['products'][]);
+					print_r($info);
 					echo"</pre>";
 				}
-			}*/
-			
+			}
 		?>
 	</body>
 </html>
